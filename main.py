15b6d9a3835161631f15d8591466c1d17cb22b49
terry0w <alonsomunozlor2005@gmail.com>
2026-01-03 17:23:58 +0100

    implementacion añadir tarea y funcionalidad basica en el main

--- main.py
+++ main.py
@@ -3,3 +3,20 @@ from alumno_a.funciones import mostrar_menu, ver_tareas, añadir_tarea
 from alumno_b.funciones import marcar_completada, eliminar_tarea, despedida
 
 # Menu funcional
+numerito = 32
+while numerito == 32:
+    opcion = mostrar_menu()
+        
+    if opcion == "1":
+        ver_tareas()
+    elif opcion == "2":
+        añadir_tarea()
+    elif opcion == "3":
+        marcar_completada()
+    elif opcion == "4":
+        eliminar_tarea()
+    elif opcion == "5":
+        despedida()
+        numerito += 1
+    else:
+        print("Opción no válida")
